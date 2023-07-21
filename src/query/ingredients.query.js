f95689073d4e7705374241567ab23476508cfc96
Josh <j8shigarcia@aol.com>
2023-07-21 06:30:07 -0500

    updated queries and secret keys

--- src/query/ingredients.query.js
+++ src/query/ingredients.query.js
@@ -1,7 +1,7 @@
 const QUERY = {
     SELECT_INGREDIENTS: 'SELECT * FROM ingredients ORDER BY created_at DESC LIMIT 100',
     SELECT_INGREDIENT: 'SELECT * FROM ingredients WHERE id = ?',
-    CREATE_INGREDIENT: 'INSERT INTO ingredients(name, amount) VALUES (?, ?)',
+    CREATE_INGREDIENT: 'INSERT INTO ingredients(name) VALUES (?)',
     UPDATE_INGREDIENT: 'UPDATE ingredients SET name = ?, amount = ? WHERE id = ?',
     DELETE_INGREDIENT: 'DELETE FROM ingredients WHERE id = ?'
 }
